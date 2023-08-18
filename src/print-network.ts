1bf154a51805bf497ba950d852ab4f99541e7c80
Noah Harris <ncharris93@gmail.com>
2023-08-18 17:46:21 -0400

    fix: don't log pservs & color code log info

--- src/print-network.ts
+++ src/print-network.ts
@@ -5,14 +5,34 @@ const green = '\u001b[32m';
 const red = '\u001b[31m';
 const reset = '\u001b[0m';
 
+const getColor = (is?: boolean) => (is ? green : red);
+
 const logNode = (ns: NS, { depth, hostname }: Deep) => {
   const set = '|\t';
   const prefix = set.repeat(depth);
 
-  console.log(`${set.repeat(depth - 1)}${green + '-'.repeat(5) + reset}${green} ${hostname}${reset}`);
-  console.log(`${prefix}\t${red}${ns.getServerSecurityLevel(hostname)}${reset}`);
-  console.log(`${prefix}\t${red}${ns.getServerMoneyAvailable(hostname)}${reset}`);
-  console.log(`${prefix}\t${red}${ns.getHackTime(hostname)}${reset}`);
+  const server = ns.getServer(hostname);
+  const numReqPorts = server.numOpenPortsRequired || 0;
+  const openPorts = server.openPortCount || 0;
+  const hasEnoughPortsToHack = openPorts >= numReqPorts;
+
+  const c = {
+    port: getColor(hasEnoughPortsToHack),
+    bkdr: getColor(server.backdoorInstalled),
+    hack: getColor((server.requiredHackingSkill || 0) <= ns.getHackingLevel()),
+    admin: getColor(server.hasAdminRights),
+  };
+
+  console.log(`${set.repeat(depth - 1)}${cyan + '-'.repeat(5) + reset} ${hostname}${reset}`);
+  console.log(`${prefix}\t${red}SEC: ${ns.getServerSecurityLevel(hostname)}${reset}`);
+  console.log(
+    `${prefix}\t${red}$$$: ${ns.formatNumber(ns.getServerMoneyAvailable(hostname) || 0, 3, 0, true)}${reset}`,
+  );
+  console.log(`${prefix}\t${red}Htime : ${ns.getHackTime(hostname)}${reset}`);
+  console.log(`${prefix}\t${c.port}REQ ports : ${ns.getServerNumPortsRequired(hostname)}${reset}`);
+  console.log(`${prefix}\t${c.hack}REQ hack : ${ns.getServerRequiredHackingLevel(hostname)}${reset}`);
+  console.log(`${prefix}\t${c.bkdr}BKDR: ${server.backdoorInstalled}${reset}`);
+  console.log(`${prefix}\t${c.admin}Admin: ${server.hasAdminRights}${reset}`);
 };
 
 type Deep = {
@@ -23,7 +43,10 @@ type Deep = {
 const getDeep = (names: string[], depth: number) => names.map((name) => ({ hostname: name, depth }));
 
 export function printNodeNetwork(ns: NS) {
-  const queue: Deep[] = ns.scan().map((name) => ({ hostname: name, depth: 1 }));
+  const queue: Deep[] = ns
+    .scan()
+    .filter((name) => !name.includes('pserv-'))
+    .map((name) => ({ hostname: name, depth: 1 }));
   const processedNodes: string[] = ['home'];
   console.log(queue);
   while (queue.length) {
@@ -38,4 +61,7 @@ export function printNodeNetwork(ns: NS) {
     const newDeep = getDeep(newNodes, host.depth + 1);
     queue.unshift(...newDeep);
   }
+}
+export async function main(ns: NS) {
+  printNodeNetwork(ns);
 }
