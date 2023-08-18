ece9bd2ebe4f871462ae13b2cf07666f33601079
Noah Harris <ncharris93@gmail.com>
2023-08-18 09:45:55 -0400

    chore: rm comments

--- src/early-hack-template.ts
+++ src/early-hack-template.ts
@@ -2,14 +2,11 @@ import { NS } from '@ns';
 
 /** @param {NS} ns */
 export async function main(ns: NS) {
-  // Defines the "target server", which is the server
-  // that we're going to hack. In this case, it's "n00dles"
   const target = `${ns.args[0]}`;
 
   if (!ns.hasRootAccess(target)) {
     return console.log(`[EHT] No root access for: ${target}`);
   }
-  //const server = ns.getServer()
 
   const log = (...args: any[]) => {
     if (args.length === 1) {
@@ -21,18 +18,11 @@ export async function main(ns: NS) {
 
   log('Init Hacking Template: ', target);
 
-  // Defines how much money a server should have before we hack it
-  // In this case, it is set to the maximum amount of money.
   const maxServerMoney = ns.getServerMaxMoney(target);
-
-  // Defines the maximum security level the target server can
-  // have. If the target's security level is higher than this,
-  // we'll weaken it before doing anything else
   const securityThresh = ns.getServerMinSecurityLevel(target);
 
   let scriptLoopTime = Date.now();
 
-  // Infinite loop that continously hacks/grows/weakens the target server
   while (true) {
     const startTime = Date.now();
     const endTime = () => Date.now() - startTime;
