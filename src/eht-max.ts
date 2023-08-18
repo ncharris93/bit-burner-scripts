ea8026c0db2440cb1821aa63250d6ee840af43b7
Noah Harris <ncharris93@gmail.com>
2023-08-17 22:46:51 -0400

    fix: default home & use utils

--- src/eht-max.ts
+++ src/eht-max.ts
@@ -3,6 +3,7 @@
  */
 
 import { NS } from '@ns';
+import { getCurrentTarget } from './get-current-target';
 
 const SCRIPT = 'early-hack-template.js';
 
@@ -10,15 +11,18 @@ const SCRIPT = 'early-hack-template.js';
 export async function main(ns: NS) {
   //const target = ns.getServer(ns.args[0])
   const hostname = 'home';
-  const target = ns.args[0];
+  const target = getCurrentTarget(ns).name;
 
   const usedRam = ns.getServerUsedRam(hostname);
   const targetMaxRam = ns.getServerMaxRam(hostname);
+  //   const ramToKeepFree = Math.min(10, targetMaxRam * 0.1);
+  const ramToKeepFree = 10;
   const scriptMem = ns.getScriptRam(SCRIPT);
 
-  const maxThreads = Math.floor((targetMaxRam - usedRam) / scriptMem);
+  const maxThreads = Math.floor((targetMaxRam - (usedRam + ramToKeepFree)) / scriptMem) || 1;
 
   console.log({ targetMaxRam, scriptMem, maxThreads });
 
   ns.exec(SCRIPT, hostname, maxThreads, target);
+  console.log(`${hostname} memory left: ${ns.getServerUsedRam(hostname)}`);
 }
