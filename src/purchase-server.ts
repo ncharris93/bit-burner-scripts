5287f245728310c88cdbdb384be34a510f6d89ed
Noah Harris <ncharris93@gmail.com>
2023-08-18 11:06:32 -0400

    fix: fix naming of purchased servers

--- src/purchase-server.ts
+++ src/purchase-server.ts
@@ -3,23 +3,23 @@ import { getCurrentTarget } from './get-current-target';
 
 /** @param {NS} ns */
 export async function main(ns: NS) {
-  // How much RAM each purchased server will have. In this case, it'll
-  // be 8GB.
   const ram = ns.args[0] as number;
 
-  // Iterator we'll use for our loop
   let i = 0;
+  // bump i for each server already purchased at the given RAM amount
+  ns.scan().forEach((hostname) => {
+    if (hostname.includes('pserv')) {
+      if (ns.getServerMaxRam(hostname) === ram) {
+        i++;
+      }
+    }
+  });
 
   // Continuously try to purchase servers until we've reached the maximum
   // amount of servers
   while (i < ns.getPurchasedServerLimit()) {
     // Check if we have enough money to purchase a server
     if (ns.getServerMoneyAvailable('home') > ns.getPurchasedServerCost(ram)) {
-      // If we have enough money, then:
-      //  1. Purchase the server
-      //  2. Copy our hacking script onto the newly-purchased server
-      //  3. Run our hacking script on the newly-purchased server with 3 threads
-      //  4. Increment our iterator to indicate that we've bought a new server
       const hostname = ns.purchaseServer(`pserv-${ram}-${i}`, +ram);
       if (!hostname) {
         console.log('NO SERVER??', hostname);
