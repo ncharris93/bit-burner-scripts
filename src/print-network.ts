26a807f5e7af29be1edce5bd04e8d4fa6d97dafa
Noah Harris <ncharris93@gmail.com>
2023-08-19 10:37:57 -0400

    refactor: rename hostname » host

--- src/print-network.ts
+++ src/print-network.ts
@@ -7,32 +7,33 @@ const reset = '\u001b[0m';
 
 const getColor = (is?: boolean) => (is ? green : red);
 
-const logNode = (ns: NS, { depth, hostname }: Deep) => {
+const logNode = (ns: NS, { depth, hostname: host }: Deep) => {
   const set = '|\t';
   const prefix = set.repeat(depth);
 
-  const server = ns.getServer(hostname);
+  const server = ns.getServer(host);
   const numReqPorts = server.numOpenPortsRequired || 0;
   const openPorts = server.openPortCount || 0;
   const hasEnoughPortsToHack = openPorts >= numReqPorts;
+  const moneyTimeRato = ns.getServerMaxMoney(host) / ns.getHackTime(host);
 
   const c = {
     port: getColor(hasEnoughPortsToHack),
     bkdr: getColor(server.backdoorInstalled),
     hack: getColor((server.requiredHackingSkill || 0) <= ns.getHackingLevel()),
     admin: getColor(server.hasAdminRights),
+    mtr: reset,
   };
 
-  console.log(`${set.repeat(depth - 1)}${cyan + '-'.repeat(5) + reset} ${hostname}${reset}`);
-  console.log(`${prefix}\t${red}SEC: ${ns.getServerSecurityLevel(hostname)}${reset}`);
-  console.log(
-    `${prefix}\t${red}$$$: ${ns.formatNumber(ns.getServerMoneyAvailable(hostname) || 0, 3, 0, true)}${reset}`,
-  );
-  console.log(`${prefix}\t${red}Htime : ${ns.getHackTime(hostname)}${reset}`);
-  console.log(`${prefix}\t${c.port}REQ ports : ${ns.getServerNumPortsRequired(hostname)}${reset}`);
-  console.log(`${prefix}\t${c.hack}REQ hack : ${ns.getServerRequiredHackingLevel(hostname)}${reset}`);
+  console.log(`${set.repeat(depth - 1)}${cyan + '-'.repeat(5) + reset} ${host}${reset}`);
+  console.log(`${prefix}\t${red}SEC: ${ns.getServerSecurityLevel(host)}${reset}`);
+  console.log(`${prefix}\t${red}$$$: ${ns.formatNumber(ns.getServerMoneyAvailable(host) || 0, 3, 0, true)}${reset}`);
+  console.log(`${prefix}\t${red}Htime : ${ns.getHackTime(host)}${reset}`);
+  console.log(`${prefix}\t${c.port}REQ ports : ${ns.getServerNumPortsRequired(host)}${reset}`);
+  console.log(`${prefix}\t${c.hack}REQ hack : ${ns.getServerRequiredHackingLevel(host)}${reset}`);
   console.log(`${prefix}\t${c.bkdr}BKDR: ${server.backdoorInstalled}${reset}`);
   console.log(`${prefix}\t${c.admin}Admin: ${server.hasAdminRights}${reset}`);
+  console.log(`${prefix}\t${c.mtr}$/T: ${moneyTimeRato}${reset}`);
 };
 
 type Deep = {
