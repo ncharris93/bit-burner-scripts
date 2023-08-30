7320df4404d84e426f8941c10b6ca25966dfc29b
Noah Harris <ncharris93@gmail.com>
2023-08-29 22:33:52 -0400

    fix: don't start script on purchase

--- src/purchase-server.ts
+++ src/purchase-server.ts
@@ -24,26 +24,28 @@ export async function main(ns: NS) {
     //  console.log({ serverCost, moneyAvailable, diff: moneyAvailable - serverCost });
     if (moneyAvailable > serverCost) {
       const hostname = ns.purchaseServer(`pserv-${ram}-${i}`, +ram);
-      if (!hostname) {
-        console.log('NO SERVER??', hostname);
-        return;
-      }
+      ns.tprint(`${hostname ? 'SUCCESS' : 'ERROR'} Server ${hostname} purchased`);
+      // if (!hostname) {
+      //   console.log('NO SERVER??', hostname);
+      //   return;
+      // }
 
-      const SCRIPT = 'early-hack-template.js';
-      ns.scp(SCRIPT, hostname);
+      // const SCRIPT = 'early-hack-template.js';
+      // ns.scp(SCRIPT, hostname);
 
-      const scriptMemUsed = ns.getScriptRam(SCRIPT);
-      const maxThreads = Math.floor(ns.getServerMaxRam(hostname) / scriptMemUsed) || 1;
+      // const scriptMemUsed = ns.getScriptRam(SCRIPT);
+      // const maxThreads = Math.floor(ns.getServerMaxRam(hostname) / scriptMemUsed) || 1;
 
-      if (ram < 64) {
-        ns.exec('early-hack-template.js', hostname, maxThreads, getCurrentTarget(ns)?.name);
-      } else {
-        ns.exec('init-server-orchestrator.js', hostname);
-      }
+      // if (ram < 64) {
+      //   ns.exec('early-hack-template.js', hostname, maxThreads, getCurrentTarget(ns)?.name);
+      // } else {
+      //   ns.exec('init-server-orchestrator.js', hostname);
+      // }
       ++i;
     }
     //Make the script wait for a second before looping again.
     //Removing this line will cause an infinite loop and crash the game.
     await ns.sleep(5_000);
   }
+  ns.tprint('INFO purchase-server.js fin');
 }
